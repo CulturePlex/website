86eea1c6e85affb62013b4ca59a05baae7db9b8d
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-01-10 22:12:56 +0100

    Added first skeleton to Fiber backend

---
+++ cultureplex/web/admin.py
@@ -0,0 +1,8 @@
+from web.models import Project
+from web.models import Publication
+from web.models import People
+from django.contrib import admin
+
+admin.site.register(Project)
+admin.site.register(Publication)
+admin.site.register(People)
---
+++ cultureplex/web/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    contact = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    active = models.IntegerField()
+
+class Publication(models.Model):
+    authors = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
+    publication_type = models.CharField(max_length=200)
+    pdf = models.CharField(max_length=200)
+
+class People(models.Model):
+    name = models.CharField(max_length=200)
+    contact = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    blog = models.CharField(max_length=200)
+    social = models.CharField(max_length=200)
+
