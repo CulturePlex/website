fe63607baa7e68838948cd089e013288430be147
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2012-01-16 17:45:10 +0100

    Added external_authors and internal_authors to Publication model

--- cultureplex/web/models.py
+++ cultureplex/web/models.py
@@ -9,20 +9,6 @@ class Project(models.Model):
     def __unicode__(self):
         return self.name
 
-class Publication(models.Model):
-    PUB_TYPES = (
-        (u'CG', u'Congreso'),
-        (u'CF', u'Conferencia'),
-        (u'W', u'Workshop'),
-    )
-    authors = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    year = models.CharField(max_length=200)
-    publication_type = models.CharField(max_length=200, choices=PUB_TYPES)
-    pdf = models.CharField(max_length=200)
-    def __unicode__(self):
-        return self.title
-
 class Person(models.Model):
     name = models.CharField(max_length=200)
     contact = models.CharField(max_length=200)
@@ -32,3 +18,17 @@ class Person(models.Model):
     def __unicode__(self):
         return self.name
 
+class Publication(models.Model):
+    PUB_TYPES = (
+        (u'CG', u'Congreso'),
+        (u'CF', u'Conferencia'),
+        (u'W', u'Workshop'),
+    )
+    external_authors = models.CharField(max_length=200)
+    internal_authors = models.ForeignKey(Person)
+    title = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
+    publication_type = models.CharField(max_length=200, choices=PUB_TYPES)
+    pdf = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.title
