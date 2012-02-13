41ed5e24d23ccfc528efdd3733a835222bc122a2
Javier de la Rosa <versae@gmail.com>
2012-02-13 14:33:39 -0500

    Removing fiber

--- cultureplex/fiber/fiber_admin/options.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.admin import ModelAdmin as DjangoModelAdmin
-from mptt.admin import MPTTModelAdmin as DjangoMPTTModelAdmin
-
-
-class ModelAdmin(DjangoModelAdmin):
-    pass
-
-class MPTTModelAdmin(DjangoMPTTModelAdmin):
-    pass
--- cultureplex/fiber/static/fiber/js/admin-extra.js
+++
@@ -1,2 +0,0 @@
-window.CKEDITOR_CONFIG_FORMAT_TAGS = 'div;p;h1;h2;h3;h4';
-
